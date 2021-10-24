44106dd1b71a1d915731d0369ecb23e73906ce14
locoxxxle <leandroxxx@hotmail.com>
2021-10-24 01:02:07 -0400

    Update main.js

--- src/week_146/main.js
+++ src/week_146/main.js
@@ -181,3 +181,24 @@ function easeInOutCubic(t, b, c, d) {
   if ((t/=d/2) < 1) return c/2*t*t*t + b;
   return c/2*((t-=2)*t*t + 2) + b;
 }
+
+function btnSVGExportClick(){
+  var rendererSVG = new THREE.SVGRenderer();
+  rendererSVG.setSize(window.innerWidth, window.innerHeight);
+  rendererSVG.render( scene, camera );
+  ExportToSVG(rendererSVG, "test.svg");
+}
+function ExportToSVG(rendererSVG, filename){
+  var XMLS = new XMLSerializer();
+  var svgfile = XMLS.serializeToString(rendererSVG.domElement);
+  var svgData = svgfile;
+  var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+  var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+  var svgUrl = URL.createObjectURL(svgBlob);
+  var downloadLink = document.createElement("a");
+  downloadLink.href = svgUrl;
+  downloadLink.download = filename;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
