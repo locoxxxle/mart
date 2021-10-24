31d9bd3e0ed00d75f6a59c0c357151a2722467b2
locoxxxle <leandroxxx@hotmail.com>
2021-10-24 01:22:21 -0400

    Update main.js

--- src/week_146/main.js
+++ src/week_146/main.js
@@ -166,6 +166,30 @@ const tick = () => {
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
+
+  function btnSVGExportClick(){
+    var rendererSVG = new THREE.SVGRenderer();
+    rendererSVG.setSize(window.innerWidth, window.innerHeight);
+    rendererSVG.render( scene, camera );
+    ExportToSVG(rendererSVG, "test.svg");
+  }
+  function ExportToSVG(rendererSVG, filename){
+    var XMLS = new XMLSerializer();
+    var svgfile = XMLS.serializeToString(rendererSVG.domElement);
+    var svgData = svgfile;
+    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+    var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = filename;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+
+
 };
 
 tick();
@@ -182,23 +206,3 @@ function easeInOutCubic(t, b, c, d) {
   return c/2*((t-=2)*t*t + 2) + b;
 }
 
-function btnSVGExportClick(){
-  var rendererSVG = new THREE.SVGRenderer();
-  rendererSVG.setSize(window.innerWidth, window.innerHeight);
-  rendererSVG.render( scene, camera );
-  ExportToSVG(rendererSVG, "test.svg");
-}
-function ExportToSVG(rendererSVG, filename){
-  var XMLS = new XMLSerializer();
-  var svgfile = XMLS.serializeToString(rendererSVG.domElement);
-  var svgData = svgfile;
-  var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-  var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
-  var svgUrl = URL.createObjectURL(svgBlob);
-  var downloadLink = document.createElement("a");
-  downloadLink.href = svgUrl;
-  downloadLink.download = filename;
-  document.body.appendChild(downloadLink);
-  downloadLink.click();
-  document.body.removeChild(downloadLink);
-}
